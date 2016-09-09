ece06e89b476cec81994fa97b1a6340efd68d330
zackramjan <zack.hr@gmail.com>
2016-09-09 14:53:20 -0400

    java data model

---
+++ src/org/statehub/client/data/Feature.java
@@ -0,0 +1,12 @@
+package org.statehub.client.data;
+
+import java.io.Serializable;
+
+public class Feature implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+	String name;
+	Float score;
+	Tags tags;
+
+}
---
+++ src/org/statehub/client/data/Tags.java
@@ -0,0 +1,9 @@
+package org.statehub.client.data;
+
+import java.util.ArrayList;
+
+public class Tags extends ArrayList<String>
+{
+	private static final long serialVersionUID = 1L;
+
+}
