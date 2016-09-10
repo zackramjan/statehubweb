e0ecfae7f503d02137fc191cf9303b5d8956e226
zack <zack@192.168.1.82>
2016-09-10 00:04:23 -0400

    serialization to db code

--- src/org/statehub/client/data/Model.java
+++ src/org/statehub/client/data/Model.java
@@ -1,14 +1,14 @@
 package org.statehub.client.data;
 import java.io.Serializable;
+import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.Date;
 
 public class Model implements Serializable
 {
 	private static final long serialVersionUID = 1L;
 	Integer id;
 	String name;
-	Date revision;
+	Timestamp revision;
 	ArrayList<State> states;
 	Tags tags;
 	
