c4e399d033bc5f6d5290be14a1b5dfabe2bb2268
zackramjan <zack.hr@gmail.com>
2016-09-20 16:10:42 -0400

    migrated to mongodb for model backend
    added search support
    
    removed mysql deps and decided not to use morphia in favor of gson

--- src/org/statehub/client/StateHubServiceAsync.java
+++ src/org/statehub/client/StateHubServiceAsync.java
@@ -14,5 +14,5 @@ public interface StateHubServiceAsync
 		void getModel(String search, AsyncCallback<ArrayList<Model>> callback);
 		void storeModel(Model m, AsyncCallback<Integer> callback);
 		void toJson(Model m, AsyncCallback<String> callback);
-		void fromJason(String s, AsyncCallback<Model> callback);
+		void fromJson(String s, AsyncCallback<Model> callback);
 }
